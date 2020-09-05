d1673b4be6f042e214699e61ec50930de31022ef
Hiroshi Shimamoto <hshimamoto@users.noreply.github.com>
2020-09-06 08:57:17 +0900

    set log prefix

--- cmd/1bin/main.go
+++ cmd/1bin/main.go
@@ -4,6 +4,7 @@
 package main
 
 import (
+    "fmt"
     "log"
     "os"
     "path/filepath"
@@ -14,7 +15,10 @@ import (
 
 func main() {
     cmd := filepath.Base(os.Args[0])
-    log.Printf("1bin: %s\n", cmd)
+    pid := os.Getpid()
+    log.Printf("1bin: %d <%s>\n", pid, cmd)
+    log.SetFlags(log.Flags() | log.Lmsgprefix)
+    log.SetPrefix(fmt.Sprintf("[%d <%s>] ", pid, cmd))
     switch cmd {
     case "fwd": fwd.Run(os.Args[1:])
     case "fwdset": fwdset.Run(os.Args[1:])
