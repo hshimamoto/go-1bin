a2e5153abae36ee330097ac1ef68df2a3c975d32
Hiroshi Shimamoto <hshimamoto@users.noreply.github.com>
2020-09-09 07:36:14 +0900

    suppress first log on some commands

--- cmd/1bin/main.go
+++ cmd/1bin/main.go
@@ -19,7 +19,11 @@ import (
 func main() {
     cmd := filepath.Base(os.Args[0])
     pid := os.Getpid()
-    log.Printf("1bin: %d <%s>\n", pid, cmd)
+    switch cmd {
+    case "bashhistcompact":
+    default:
+	log.Printf("1bin: %d <%s>\n", pid, cmd)
+    }
     log.SetFlags(log.Flags() | log.Lmsgprefix)
     log.SetPrefix(fmt.Sprintf("[%d <%s>] ", pid, cmd))
     switch cmd {
