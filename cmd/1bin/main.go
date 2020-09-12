3355975c206e148a2a7dee0ba0e97e8e440c5382
Hiroshi Shimamoto <hshimamoto@users.noreply.github.com>
2020-09-12 09:25:52 +0900

    introduce command struct

--- cmd/1bin/main.go
+++ cmd/1bin/main.go
@@ -18,23 +18,52 @@ import (
     "1bin/sshproxy"
 )
 
+type command struct {
+    name string
+    run func(args []string)
+    noheader bool
+}
+
+var cmds = []command{
+    command{ name: "bashhistcompact", run: bashhistcompact.Run, noheader: true },
+    command{ name: "fwd", run: fwd.Run },
+    command{ name: "fwdset", run: fwdset.Run },
+    command{ name: "golangbuilder", run: golangbuilder.Run },
+    command{ name: "gradlebuilder", run: gradlebuilder.Run },
+    command{ name: "sshfwd", run: sshfwd.Run },
+    command{ name: "sshproxy", run: sshproxy.Run },
+}
+
+func header(cmd string, pid int) {
+    log.Printf("1bin: multiple tools in one binary\n")
+    log.Printf("1bin: <%s> pid=%d\n", cmd, pid)
+}
+
 func main() {
     cmd := filepath.Base(os.Args[0])
     pid := os.Getpid()
-    switch cmd {
-    case "bashhistcompact":
-    default:
-	log.Printf("1bin: %d <%s>\n", pid, cmd)
+
+    // lookup cmd
+    var c *command = nil
+    for _, e := range cmds {
+	if e.name == cmd {
+	    c = &e
+	    break
+	}
     }
+    if c == nil {
+	header(cmd, pid)
+    }
+
+    // show header
+    if !c.noheader {
+	header(cmd, pid)
+    }
+
+    // setup logger
     log.SetFlags(log.Flags() | log.Lmsgprefix)
     log.SetPrefix(fmt.Sprintf("[%d <%s>] ", pid, cmd))
-    switch cmd {
-    case "bashhistcompact": bashhistcompact.Run(os.Args[1:])
-    case "fwd": fwd.Run(os.Args[1:])
-    case "fwdset": fwdset.Run(os.Args[1:])
-    case "golangbuilder": golangbuilder.Run(os.Args[1:])
-    case "gradlebuilder": gradlebuilder.Run(os.Args[1:])
-    case "sshfwd": sshfwd.Run(os.Args[1:])
-    case "sshproxy": sshproxy.Run(os.Args[1:])
-    }
+
+    // run command
+    c.run(os.Args[1:])
 }
